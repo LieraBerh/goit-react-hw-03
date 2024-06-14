0a403a88c6d57906e4e411f50d16cbbfde28efe3
LieraBerh <lierrastern@gmail.com>
2024-06-14 10:49:54 +0200

    add formik form

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,44 +1,52 @@
-import { useState } from "react";
-
 /* eslint-disable react/prop-types */
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 const ContactForm = ({ addContact }) => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+  const handleSubmit = (values, { resetForm }) => {
+    addContact(values.name, values.number);
+    resetForm();
   };
 
-  const handleNumberChange = (e) => {
-    setNumber(e.target.value);
-  };
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
 
-  const submitForm = (e) => {
-    e.preventDefault();
-
-    addContact(name, number);
-
-    setName("");
-
-    setNumber("");
+  const initialValues = {
+    name: "",
+    number: "",
   };
 
   return (
-    <form onSubmit={submitForm}>
-      <input
-        type="text"
-        placeholder="Enter name"
-        value={name}
-        onChange={handleNameChange}
-      />
-      <input
-        type="text"
-        placeholder="Enter number"
-        value={number}
-        onChange={handleNumberChange}
-      />
-      <button type="submit">Add Contact</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className="form">
+        <label className="label">
+          <span>Name</span>
+          <Field name="name" placeholder="Enter name" className="input" />
+          <ErrorMessage name="name" component="span" className="errorMsg" />
+        </label>
+        <label className="label">
+          <span>Number</span>
+          <Field name="number" placeholder="Enter number" className="input" />
+          <ErrorMessage name="number" component="span" className="errorMsg" />
+        </label>
+        <button className="submitBtn" type="submit">
+          Add Contact
+        </button>
+      </Form>
+    </Formik>
   );
 };
+
 export default ContactForm;
